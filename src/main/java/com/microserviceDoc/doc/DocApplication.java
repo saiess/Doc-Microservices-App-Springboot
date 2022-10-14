b97469fe15e45242a8d199244bc43afb6b6390e6
saiess <saidessalami0@gmail.com>
2022-10-14 17:13:52 +0100

    structure setup

---
+++ src/main/java/com/microserviceDoc/doc/DocApplication.java
@@ -0,0 +1,13 @@
+package com.microserviceDoc.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceDoc/doc/DocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceDoc.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
