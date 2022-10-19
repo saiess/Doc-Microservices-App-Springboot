042c287ce5f112c0cfe3079808f5a6ae9d8e5a94
saiess <saidessalami0@gmail.com>
2022-10-19 10:06:51 +0100

    toke service logic

--- src/main/java/com/microserviceDoc/doc/services/TokenService.java
+++ src/main/java/com/microserviceDoc/doc/services/TokenService.java
@@ -1,10 +1,9 @@
 package com.microserviceDoc.doc.services;
 
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+
 public interface TokenService {
 
     String getUserId(String jwtToken);
--- src/main/java/com/microserviceDoc/doc/services/impl/TokenServiceImpl.java
+++ src/main/java/com/microserviceDoc/doc/services/impl/TokenServiceImpl.java
@@ -1,17 +1,37 @@
 package com.microserviceDoc.doc.services.impl;
 
 import com.microserviceDoc.doc.services.TokenService;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Service
 public class TokenServiceImpl implements TokenService {
     @Override
     public String getUserId(String jwtToken) {
-        return null;
+        //abc.123.awe
+        String claims = new String(Base64.getUrlDecoder().decode(jwtToken.split("\\.")[1]));
+        JSONObject claimsJson = new JSONObject(claims);
+
+        return claimsJson.getString("iss");
     }
 
     @Override
     public List<String> getUserRoles(String jwtToken) {
-        return null;
+        String claims = new String(Base64.getUrlDecoder().decode(jwtToken.split("\\.")[1]));
+        JSONObject claimsJson = new JSONObject(claims);
+
+        // "[admin, user]"
+        String audience = claimsJson.getString("aud");
+
+        final String[] split = audience
+                .replace("[", "")
+                .replace("]", "")
+                .split(",");
+        return Stream.of(split).map(String::trim).collect(Collectors.toList());
     }
 }
